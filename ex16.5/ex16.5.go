926f3891516a8404ecf0be8248e3082ca084c776
parkjunhyeong0203 <pjhqw030203@naver.com>
2026-06-22 23:32:05 +0900

    module, pkg example

---
+++ ex16.5/ex16.5.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"ex16.5/exinit"
+)
+
+func main() {
+	fmt.Println("main function")
+	exinit.Printd()
+
+}
---
+++ usepkg/customphg/customphg.go
@@ -0,0 +1,7 @@
+package customphg
+
+import "fmt"
+
+func PrintCustom() {
+	fmt.Println("This is custom package!")
+}
